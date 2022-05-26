b5b5b1390676819512fb6602ec6bd35316d9942d
Benedicte Raae <raae@raae.codes>
2022-05-26 20:26:20 +0200

    feat: source cloudinary resources

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,11 +6,28 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const createCloudinaryNodes = async () => {
+const createCloudinaryNodes = async (gatsbyUtils) => {
   const { actions, reporter, createNodeId, createContentDigest } = gatsbyUtils;
   const { createNode } = actions;
 
-  // Let get to work!
+  const result = await cloudinary.api.resources({
+    resource_type: "image",
+  });
+
+  reporter.info(`Fetched Cloudinary Assets >>> ${result.resources.length}`);
+
+  result.resources.forEach((resource) => {
+    reporter.info(`Create CloudinaryAsset >>> ${resource.public_id}`);
+    createNode({
+      id: createNodeId(resource.public_id),
+      ...resource,
+      internal: {
+        type: "CloudinaryAsset",
+        content: JSON.stringify(resource),
+        contentDigest: createContentDigest(resource),
+      },
+    });
+  });
 };
 
 exports.sourceNodes = async (gatsbyUtils) => {
